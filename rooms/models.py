ad9c6cc9bbb13d430f508eeed1faa7d586e43950
yhkim <huvitzedger@gmail.com>
2021-01-26 16:47:31 +0900

    #12 DETAILVIEW

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 
 from core import models as core_models
@@ -105,6 +106,10 @@ class Room(core_models.TimeStampedModel):
         super().save(*arg, **kwargs)   #call the real save() method
         print("room- saved")
     
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
+    
+
     def total_rating(self):
         all_reviews = self.reviews.all()
         all_ratings = 0
--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,18 @@
 from django.utils import timezone
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
+from django.urls import reverse
+from django.http import Http404
+from django import forms
+
+
+class SearchForm(forms.Form):
+
+    city = forms.CharField(initial="Anywhere")
+    price = forms.IntegerField(required=False)
+    room_type = forms.ModelChoiceField(queryset=models.RoomType.objects.all())
+
 
 class HomeView(ListView):
 
@@ -29,5 +40,19 @@ class HomeView(ListView):
 """
 
 def room_detail(request, pk):
-  print(pk)
-  return render(request, "rooms/detail.html")
+  #print(pk)
+
+  try:
+    room = models.Room.objects.get(pk=pk) 
+    return render(request, "rooms/detail.html", {"room": room})
+  except models.Room.DoesNotExist:
+    raise Http404()
+    #return redirect(reverse("core:home"))
+
+
+#class RoomDetail(DetailView):
+
+#""" RoomDetail Definition """
+
+#model = models.Room
+
